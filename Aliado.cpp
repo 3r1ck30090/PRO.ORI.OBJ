3348476f4a788ebfccf7a9a840dd3696a26bded8
3rick30090 <erickbu2007@gmail.com>
2026-05-28 18:22:43 -0600

    Update Aliado.cpp

--- Aliado.cpp
+++ Aliado.cpp
@@ -3,3 +3,10 @@
 Aliado::Aliado(string n, int v, int a, string e)
 : Personaje(n, v, a, e) {
 }
+
+void Aliado::atacar(Personaje &objetivo) {
+
+    cout << nombre << " ataca!\n";
+
+    objetivo.recibirDanio(ataque + 5);
+}
