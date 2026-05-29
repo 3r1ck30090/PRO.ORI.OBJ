f25fe78556cb91103a40fbd7e479c17bbc72a7ac
3rick30090 <erickbu2007@gmail.com>
2026-05-28 18:24:02 -0600

    Implement attack method in Enemigo class
    Added attack method to Enemigo class.

--- Enemigo.cpp
+++ Enemigo.cpp
@@ -3,3 +3,10 @@
 Enemigo::Enemigo(string n, int v, int a, string e)
 : Personaje(n, v, a, e) {
 }
+
+void Enemigo::atacar(Personaje &objetivo) {
+
+    cout << nombre << " ataca agresivamente ñojao >:( !\n";
+
+    objetivo.recibirDanio(ataque + 10);
+}
